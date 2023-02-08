e13a9674d3c0a3fd3e7a3bf89d443b3110dd903f
dev <remnev.dev@gmail.com>
2023-02-08 12:30:04 +0300

    feat: add shop block

---
+++ src/js/data.js
@@ -0,0 +1,7 @@
+const dataCards = [
+	{ title: 'Nike', price: 100 },
+	{ title: 'Nord Face', price: 300 },
+	{ title: 'Leica', price: 500 },
+];
+
+export default dataCards;
---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+import dataCards from './data.js';
+
+const shop = document.querySelector('.shop');
+function addCardsToShop(item) {
+	const templateCards = document.querySelector('.cards').content;
+	const card = templateCards.querySelector('.card').cloneNode(true);
+
+	const cardTitel = card.querySelector('.card__title');
+	const cardPriceNumber = card.querySelector('.card__price-number');
+	cardTitel.textContent = item.title;
+	cardPriceNumber.textContent = item.price;
+
+	return card;
+}
+
+function addCards(item, container) {
+	const newCard = addCardsToShop(item);
+	container.append(newCard);
+}
+
+dataCards.forEach(card => {
+	addCards(card, shop);
+});
