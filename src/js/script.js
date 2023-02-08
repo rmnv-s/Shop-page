4e8ec34572cdab88c44f9460bd28d84b04a4a5bc
dev <remnev.dev@gmail.com>
2023-02-08 13:48:39 +0300

    feat: add cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 import dataCards from './data.js';
 
-const shop = document.querySelector('.shop');
+const shop = document.querySelector('.cards');
+
 function addCardsToShop(item) {
-	const templateCards = document.querySelector('.cards').content;
+	const templateCards = document.querySelector('.template').content;
 	const card = templateCards.querySelector('.card').cloneNode(true);
 
 	const cardTitel = card.querySelector('.card__title');
