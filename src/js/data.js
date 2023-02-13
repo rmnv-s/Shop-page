e57c23bd2cdb70f928434b35ab2268aebb27ded2
dev <remnev.dev@gmail.com>
2023-02-13 15:26:47 +0300

    fix: build

--- src/js/data.js
+++
@@ -1,7 +0,0 @@
-const dataCards = [
-	{ title: 'Nike', price: 100 },
-	{ title: 'Nord Face', price: 300 },
-	{ title: 'Leica', price: 500 },
-];
-
-export default dataCards;
--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,33 @@
-import dataCards from './data.js';
+const dataCards = [
+  { title: "Nike", price: 100 },
+  { title: "Nord Face", price: 300 },
+  { title: "Leica", price: 500 },
+];
 
 //  приходят карточки товаров
-const shop = document.querySelector('.cards');
+const shop = document.querySelector(".cards");
 
 function addCardsToShop(item) {
-	const templateCards = document.querySelector('.template').content;
-	const card = templateCards.querySelector('.card').cloneNode(true);
+  const templateCards = document.querySelector(".template").content;
+  const card = templateCards.querySelector(".card").cloneNode(true);
 
-	const cardTitel = card.querySelector('.card__title');
-	const cardPriceNumber = card.querySelector('.card__price-number');
-	cardTitel.textContent = item.title;
-	cardPriceNumber.textContent = item.price;
+  const cardTitel = card.querySelector(".card__title");
+  const cardPriceNumber = card.querySelector(".card__price-number");
+  cardTitel.textContent = item.title;
+  cardPriceNumber.textContent = item.price;
 
-	// Button «Just buy»
-	card.querySelector('.card__btn').addEventListener('click', () => {
-		console.log('click');
-	});
+  // Button «Just buy»
+  card.querySelector(".card__btn").addEventListener("click", () => {
+    console.log("click");
+  });
 
-	return card;
+  return card;
 }
-
 function addCards(item, container) {
-	// const newCard = addCardsToShop(item);
-	container.append(addCardsToShop(item));
+  // const newCard = addCardsToShop(item);
+  container.append(addCardsToShop(item));
 }
 
-dataCards.forEach(card => {
-	addCards(card, shop);
+dataCards.forEach((card) => {
+  addCards(card, shop);
 });
