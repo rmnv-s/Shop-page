cf9ae045f79664dcca6a522f3ab62f0895177ac4
dev <remnev.dev@gmail.com>
2023-03-03 18:21:37 +0300

    style: add cart to page

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,56 @@
 const dataCards = [
-	{ title: 'Nike', price: 100 },
-	{ title: 'Nord Face', price: 300 },
-	{ title: 'Leica', price: 500 },
+  { id: 1, title: 'Nike', price: 100 },
+  { id: 2, title: 'Nord Face', price: 300 },
+  { id: 3, title: 'Leica', price: 500 },
 ];
 
 //  приходят карточки товаров
 const shop = document.querySelector('.cards');
 
 function addCardsToShop(item) {
-	const templateCards = document.querySelector('.template').content;
-	const card = templateCards.querySelector('.card').cloneNode(true);
+  const templateCards = document.querySelector('.template').content;
+  const card = templateCards.querySelector('.card').cloneNode(true);
 
-	const cardTitel = card.querySelector('.card__title');
-	const cardPriceNumber = card.querySelector('.card__price-number');
-	cardTitel.textContent = item.title;
-	cardPriceNumber.textContent = item.price;
+  const cardTitel = card.querySelector('.card__title');
+  const cardPriceNumber = card.querySelector('.card__price-number');
+  cardTitel.textContent = item.title;
+  cardPriceNumber.textContent = item.price;
 
-	// Button «Just buy»
-	card.querySelector('.card__btn').addEventListener('click', () => {
-		console.log('click');
-		// addCardToBasket()
-	});
+  // Button «Just buy»
+  card.querySelector('.card__btn').addEventListener('click', () => {
+    cardToBasket(item);
+    // addToCa(item)
+  });
 
-	return card;
+  return card;
 }
 function addCards(item, container) {
-	// const newCard = addCardsToShop(item);
-	container.append(addCardsToShop(item));
+  container.append(addCardsToShop(item));
 }
 
-dataCards.forEach(card => {
-	addCards(card, shop);
+dataCards.forEach((card) => {
+  addCards(card, shop);
 });
 
 // CART
-function addCardToBasket() {
-	const templateProduct = document.querySelector('.template-product').content;
-	const product = templateProduct.querySelector('.product').cloneNode(true);
+const cartArray = [];
+const cartInner = document.querySelector('.cart__inner');
+function cardToBasket(item) {
+  const templateProduct = document.querySelector('.template-product').content;
+  const product = templateProduct.querySelector('.product').cloneNode(true);
+
+  product.querySelector('.product__title').textContent = item.title;
+  product.querySelector('.product__price').textContent = item.price;
+  product.querySelector('.product__remove').addEventListener('click', (evt) => {
+    evt.target.closest('.product').remove();
+    console.log(product);
+  });
+
+  if (cartArray.find((i) => i.id === item.id)) {
+    alert('product added');
+  } else {
+    cartInner.append(product);
+    cartArray.push(item);
+    console.log(item.id);
+  }
 }
